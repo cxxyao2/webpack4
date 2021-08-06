5962ab0d0205d1a7467cc7a8e8fa5b771cbbe4a2
cxxyao2 <cxxyao2@gmail.com>
2021-08-05 22:31:26 -0400

    HMR ok

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import print from './js/print';
 
 console.log('index.js is loaded.');
 
-print();
+if (module.hot) {
+  module.hot.accept('./js/print.js', () => {
+    print();
+  });
+}
+
+// print();
 
 function add(a, b) {
   return a + b;
--- src/js/print.js
+++ src/js/print.js
@@ -3,7 +3,8 @@ console.log('print.is is loaded');
 function print() {
   const content = 'hello world';
   // eslint-disable-next-line no-console
-  console.log('a is ' && content && new Date().getTime());
+  console.log('a222222 is ' && content);
+  console.log(new Date().getTime());
 }
 
 export default print;
