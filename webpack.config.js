24de7fbf6ccda34c8daa7b97f4325a7ad7414387
cxxyao2 <cxxyao2@gmail.com>
2021-08-06 20:26:21 -0400

    externals in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,143 +1,25 @@
-//  PWA : progressive web application
-// offline, website is accessible
-// tools: workbox --> workbox-webpack-plugin
+//  externals
 
 const { resolve } = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
-process.env.NODE_ENV = 'production';
-
-// reusable loader
-const commonCssLoader = [
-  MiniCssExtractPlugin.loader,
-  'css-loader',
-  {
-    loader: 'postcss-loader',
-    options: {
-      postcssOptions: {
-        ident: 'postcss',
-        plugins: ['postcss-preset-env'],
-      },
-    },
-  },
-];
 
 module.exports = {
   entry: ['./src/index.js'],
   output: {
-    filename: 'js/built.[contenthash:10].js',
+    filename: 'js/built.js',
     path: resolve(__dirname, 'build'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        options: {
-          fix: true,
-        },
-      },
-      {
-        oneOf: [
-          {
-            test: /\.css$/,
-            use: [...commonCssLoader],
-          },
-          {
-            test: /\.less$/,
-            use: [...commonCssLoader, 'less-loader'],
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-              /* 开启多进程打包
-              进程启动大概为600ms,进程间通讯也有时间
-              只有工作消耗时间比较长，才需要多进程打包
-              */
-              { loader: 'thread-loader', options: { workers: 2 } },
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    [
-                      '@babel/preset-env',
-                      {
-                        // load the polypill needed
-                        useBuiltIns: 'usage',
-                        corejs: { version: 3 },
-                        // compatible verison
-                        targets: {
-                          chrome: '60',
-                          firefox: '50',
-                          ie: '9',
-                          safari: '10',
-                          edge: '17',
-                        },
-                      },
-                    ],
-                  ],
-                  // babel cache
-                  // the second build will read the first cache
-                  cacheDirectory: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(jpg|png|gif)$/,
-            loader: 'url-loader',
-            options: {
-              limit: 8 * 1024,
-              name: '[hash:10].[ext]',
-              outputPath: 'imgs',
-              esModule: false,
-            },
-          },
-          {
-            test: /\.html$/,
-            loader: 'html-loader',
-          },
-          {
-            exclude: /\.(js|css|less|html|jpg|png|gif)/,
-            loader: 'file-loader',
-            options: {
-              outputPath: 'media',
-            },
-          },
-        ],
-      },
-    ],
-  },
+
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'build.[contenthash:10].css',
-    }),
-    new OptimizeCssAssetWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      // compress .html file
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-    new WorkboxWebpackPlugin.GenerateSW({
-      /* 
-      1,帮助 serviceworker快速启动
-      2,删除旧的serviceworker
-      */
-      clientsClaim: true,
-      skipWaiting: true,
     }),
   ],
 
-  // compress .js file, tree shaking
   mode: 'production',
-  devtool: 'source-map',
+  externals: {
+    // 拒绝jquery打包进来，库名和对应的包名
+    // 在index.js中引入 <script type="text/javascript" src=".."></script>
+    jquery: 'jQuery',
+  },
 };
