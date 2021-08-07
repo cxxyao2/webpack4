65df4db8afe417f94fbb62ac6be920658a3a5c0b
cxxyao2 <cxxyao2@gmail.com>
2021-08-07 15:43:59 -0400

    module config example

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,43 @@
 
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: './src/index.js',
   output: {
-    filename: 'js/built.js',
+    // 文件名称(指定名称 + 目录)
+    filename: 'js/[name].js',
+    // 输出文件目录(将来所有资源输出的公共目录)
     path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      // loader的配置
+      // 单个用loader,多个用use 【‘’，‘’】
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // only check js files under src folder
+        include: resolve(__dirname, 'src'),
+        enforce: 'pre',
+        // 延后执行 postpone
+        // enforce:'post'
+        loader: 'eslint-loader',
+        options: {},
+      },
+      {
+        // 以下配置只生效一个
+        oneOf: [],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-    }),
-    // 告诉webpack哪些苦不参与打包,同时使用的名称
-    new webpack.DllReferencePlugin({
-      manifest: resolve(__dirname, 'dll/manifest.json'),
-    }),
-    // 将某个文件打包出去，并且在index.js中自动引入
-    new AddAssetHtmlWebpackPlugin({
-      filepath: resolve(__dirname, 'dll/jquery.js'),
     }),
   ],
   mode: 'development',
